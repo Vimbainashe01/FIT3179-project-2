b8fc89a69c711fcb537a7dd2082a1b02fa2a07b8
Vimbainashe01 <146728408+Vimbainashe01@users.noreply.github.com>
2023-10-18 07:08:02 +0800

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,7 +1,8 @@
 var vg_1 = "jobs with the highest vacancies.json";
 var vg_2 = "Unemployment_rate in usa states.json";
 var vg_3 = "cost_of_living_by_state.json";
-var vg_4 = "crime_rate.json";
+var vg_4 = "Season Map.json";
+var vg_5 = "cost_of_living_2.json";
 vegaEmbed("#dot_chart", vg_1) .then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
@@ -10,10 +11,14 @@ vegaEmbed("#map", vg_2).then(function(result) {
     // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
     }).catch(console.error);
 
-vegaEmbed("#matrix", vg_3).then(function(result) {
+vegaEmbed("#matrix1", vg_3).then(function(result) {
         // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
         }).catch(console.error);
 
-vegaEmbed("#bar_chart", vg_4).then(function(result) {
+vegaEmbed("#map2", vg_4).then(function(result) {
             // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
             }).catch(console.error);
+
+vegaEmbed("#matrix2", vg_5).then(function(result) {
+                // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+                }).catch(console.error);
